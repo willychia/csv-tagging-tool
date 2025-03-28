30d0e69f73024ab19ca1cdf0f7f5e548ee2344f7
willychia <157714048+willychia@users.noreply.github.com>
2025-03-28 20:36:34 +0800

    Add Tags Filter

--- modules/data_ops.py
+++ modules/data_ops.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def get_filtered_df(keyword, exclude_keywords, selected_brands, filter_empty_feature, filter_empty_subject, filter_empty_special):
+def get_filtered_df(keyword, exclude_keywords, selected_brands, filter_empty_feature, filter_empty_subject, filter_empty_special, feature_filter, subject_filter, special_filter):
     _df = st.session_state['df'].copy()
 
     # 多關鍵字交集篩選（處理 NaN 並忽略大小寫）
@@ -10,6 +10,13 @@ def get_filtered_df(keyword, exclude_keywords, selected_brands, filter_empty_fea
         
     exclude_keywords = [k.strip() for k in exclude_keywords.split(",") if k.strip()]
     for kw in exclude_keywords:
+        _df = _df[~_df['title'].fillna("").str.contains(kw, case=False)]
+
+    for kw in [k.strip() for k in feature_filter.split(",") if k.strip()]:
+        _df = _df[~_df['title'].fillna("").str.contains(kw, case=False)]
+    for kw in [k.strip() for k in subject_filter.split(",") if k.strip()]:
+        _df = _df[~_df['title'].fillna("").str.contains(kw, case=False)]
+    for kw in [k.strip() for k in special_filter.split(",") if k.strip()]:
         _df = _df[~_df['title'].fillna("").str.contains(kw, case=False)]
 
     if selected_brands:
